a53c8ff84a946d7244606b21a317af9367729540
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-11-07 13:03:36 +0200

    fixed the logout issue when was in a nested path

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -68,7 +68,7 @@ export class AuthService {
   }
 
   logout() {
-    this.cookieService.delete(this.cookieName);
+    this.cookieService.delete(this.cookieName, '/');
     window.location.href = '/';
   }
 }
